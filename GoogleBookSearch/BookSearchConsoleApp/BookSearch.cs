3c5fc5ba2b1bb80aa5503add07a2d8c7557845d4
Davidigest <xinyustudio@gmail.com>
2014-12-18 15:17:33 +0800

    Updated GoogleBookSearch project. + Added Nuget package. + Added BookSearch.cs to the BookSearchConsoleApp project + Added testing code in Program.cs.

---
+++ GoogleBookSearch/BookSearchConsoleApp/BookSearch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Books.v1;
+using Google.Apis.Books.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace BooksAPI
+{
+    public class BookSearch
+    {
+        private static string API_KEY = "AIzaSyCg_O55PNP0OJBrqZtai5Fs1nAyIiBMX0s";
+
+        public static BooksService service = new BooksService(new BaseClientService.Initializer
+        {
+            ApplicationName = "ISBNBookSearch",
+            ApiKey = API_KEY,
+        });
+
+        public static async Task<Volume> SearchISBN(string isbn)
+        {
+            Console.WriteLine("Executing a book search request for ISBN: {0} ...", isbn);
+            var result = await service.Volumes.List(isbn).ExecuteAsync();
+            
+            if (result != null && result.Items!=null)
+            {
+                var item = result.Items.FirstOrDefault();
+                return item;
+            }
+            return null;
+        }
+
+    }
+}
---
+++ GoogleBookSearch/BookSearchConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksAPI;
+
+namespace BookSearchConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("\n------------------------------------------------------------------------");
+            
+            string isbn = "0071807993";
+            var output = BookSearch.SearchISBN(isbn);
+            var result = output.Result;
+
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine("Book Name: \t" + result.VolumeInfo.Title);
+            Console.WriteLine("Author: \t" + result.VolumeInfo.Authors.FirstOrDefault());
+            Console.WriteLine("Publisher: \t" + result.VolumeInfo.Publisher);
+            Console.WriteLine("------------------------------------------------------------------------");
+        }
+    }
+}
