9d5b4cb98f3c8f36fbb89cac9f97d56cc20bb5e4
Davidigest <xinyustudio@gmail.com>
2014-12-23 10:17:53 +0800

    Added GoogleSearch project to demonstrate Google Custom Search implementation.

---
+++ GoogleSearch/GoogleSearch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using Google.Apis.Services;
+
+namespace TestGoogleSearch
+{
+    public class GoogleSearch
+    {
+        //API Key
+        private static string API_KEY = "AIzaSyCyqAm432caVHD6ycUEWTbCNtg4rD_ao8Y";
+
+        //The custom search engine identifier
+        private static string cx = "015598178761323117960:sbbkk2__0lo";
+
+        public static CustomsearchService Service = new CustomsearchService(
+            new BaseClientService.Initializer
+        {
+            ApplicationName = "ISBNBookCsutomSearch",
+            ApiKey = API_KEY,
+        });
+
+        public static IList<Result> Search(string query)
+        {
+            Console.WriteLine("Executing google custom search for query: {0} ...", query);
+
+            CseResource.ListRequest listRequest = Service.Cse.List(query);
+            listRequest.Cx = cx;
+
+            Search search = listRequest.Execute();
+            return search.Items;
+        }
+    }
+}
---
+++ GoogleSearch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Customsearch.v1.Data;
+
+namespace TestGoogleSearch
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        string query = "The University of Hong Kong";
+        var results = GoogleSearch.Search(query);
+        foreach (Result result in results)
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Title: {0}", result.Title);
+            Console.WriteLine("Link: {0}", result.Link);
+        }
+        Console.ReadKey();
+    }
+}
+}
